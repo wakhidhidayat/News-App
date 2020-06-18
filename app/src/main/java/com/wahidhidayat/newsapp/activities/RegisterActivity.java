cf49c5176c0e67a2ddb99ecaa2dd7122fdbcab9d
Wahid Hidayat <wshidayat38@gmail.com>
2020-06-18 22:40:08 +0700

    add activity login and register

---
+++ app/src/main/java/com/wahidhidayat/newsapp/activities/RegisterActivity.java
@@ -0,0 +1,98 @@
+package com.wahidhidayat.newsapp.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.wahidhidayat.newsapp.R;
+
+import java.util.HashMap;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    MaterialEditText etName, etEmail, etPassword;
+    MaterialButton btnRegister;
+
+    FirebaseAuth auth;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etEmail = findViewById(R.id.et_email);
+        etName = findViewById(R.id.et_name);
+        etPassword = findViewById(R.id.et_password);
+        btnRegister = findViewById(R.id.btn_register);
+
+        auth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etName.getText().toString();
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if(TextUtils.isEmpty(name) || TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+                    Toast.makeText(RegisterActivity.this, "All fields are required!", Toast.LENGTH_SHORT).show();
+                } else if(password.length() < 8) {
+                    Toast.makeText(RegisterActivity.this, "Password must be at least 8 characters!", Toast.LENGTH_SHORT).show();
+                } else {
+                    register(name, email, password);
+                }
+            }
+        });
+    }
+
+    private void register(final String name, String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            // membuat akun baru
+                            FirebaseUser firebaseUser = auth.getCurrentUser();
+                            String userId = firebaseUser.getUid();
+                            reference = FirebaseDatabase.getInstance().getReference("Users").child(userId);
+
+                            // memasukkan data id dan name ke dokumen users
+                            HashMap<String, String> hashMap = new HashMap<>();
+                            hashMap.put("id", userId);
+                            hashMap.put("name", name);
+
+                            reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "Email already taken!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
