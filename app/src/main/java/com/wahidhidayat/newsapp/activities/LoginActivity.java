d3da3c86ec312fd3f27f8a77c99bbcf49fb80843
Wahid Hidayat <wshidayat38@gmail.com>
2020-07-03 23:11:01 +0700

    modify ui login

--- app/src/main/java/com/wahidhidayat/newsapp/activities/LoginActivity.java
+++ app/src/main/java/com/wahidhidayat/newsapp/activities/LoginActivity.java
@@ -3,6 +3,7 @@ package com.wahidhidayat.newsapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +15,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -38,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
     DatabaseReference reference;
     GoogleSignInClient mGoogleSignInClient;
-    SignInButton signInButton;
+    Button signInButton;
     TextView tvContinue;
     ProgressBar progressBar;
 
